6637c82ffefe27cb875d5addbc96108eb6c590bb
LeoAlias <leo.alias@gmail.com>
2021-06-14 16:19:01 -0300

    limpieza del proyecto de createReactApp

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Leo Alías FrontEnd Developer.
-        </h1>
-        <h2>
-          Linked-In: https://www.linkedin.com/in/leo-alías/
-        </h2>
-        <p>
-          Technologies I use: JS - ReactJs -Html - CSS - Jquery - Sass/Scss - NodeJs - Git/GHub among others....
-        </p>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/leo-alías"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          See My Profile
-        </a>
-      </header>
+      <h1>Hola</h1>
     </div>
   );
 }
