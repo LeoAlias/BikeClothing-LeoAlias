71f2734df752906496a58326f3d14ba3930bbf39
LeoAlias <leo.alias@gmail.com>
2021-06-15 15:41:43 -0300

    se agrego navbar con estilos y 5 secciones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import {NavBar} from './components/navbar/navbar'
 
 function App() {
+  let name = 'Leonardo'
+  let lastname = 'Alías'
+  let fullName =  `${name} ${lastname}`
+  
+  
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <NavBar />
+      {/* <h1>Hola {fullName} </h1> */}
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import './navbar.css';
+
+export const NavBar = () => {
+    return (
+        <nav className='main-nav1'>
+            <ul className='nav1'>
+                <li><a href="index">Inicio</a></li>
+                <li><a href="about">Nosotros</a></li>
+                <li><a href="products">Productos</a></li>
+                <li><a href="talles">Talles</a></li>
+                <li><a href="materiales">Materiales</a></li>
+            </ul>
+        </nav>
+    )
+}      
